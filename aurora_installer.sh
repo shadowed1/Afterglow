81ad7eb3182d564d6b59b0058a687b0c82c717ae
shadowed1 <itszachg@gmail.com>
2025-06-27 08:46:55 -0400

    Update aurora_installer.sh

--- aurora_installer.sh
+++ aurora_installer.sh
@@ -132,9 +132,9 @@ download_and_extract "$URL" "$HOME/opt/flatpak-deps"
 URL="https://archlinux.org/packages/extra/x86_64/fuse3/download"
 download_and_extract "$URL" "$HOME/opt/flatpak-deps"
 
-curl -L https://raw.githubusercontent.com/shadowed1/Aurora/main/flatpak.logic -o ~/opt/flatpak.logic
-curl -L https://raw.githubusercontent.com/shadowed1/Aurora/main/aurora -o ~/opt/bin/aurora
-curl -L https://raw.githubusercontent.com/shadowed1/Aurora/main/flatpak.env -o ~/opt/flatpak.env
+curl -L https://raw.githubusercontent.com/shadowed1/Aurora/beta/flatpak.logic -o ~/opt/flatpak.logic
+curl -L https://raw.githubusercontent.com/shadowed1/Aurora/beta/aurora -o ~/opt/bin/aurora
+curl -L https://raw.githubusercontent.com/shadowed1/Aurora/beta/flatpak.env -o ~/opt/flatpak.env
 chmod +x ~/opt/bin/aurora
 
 

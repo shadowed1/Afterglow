9df0c3cb0142839684e8710001fd1fafb93de5e8
shadowed1 <itszachg@gmail.com>
2025-06-29 19:19:27 -0400

    Update aurora_installer.sh

--- aurora_installer.sh
+++ aurora_installer.sh
@@ -95,6 +95,7 @@ download_and_extract()
     sleep 1
 }
 
+# Flatpak Core
 URL="https://archlinux.org/packages/extra/x86_64/flatpak/download"
 download_and_extract "$URL" "$HOME/opt/flatpak"
 
@@ -131,7 +132,12 @@ download_and_extract "$URL" "$HOME/opt/flatpak-deps"
 # Fastfetch
 URL="https://archlinux.org/packages/extra/x86_64/fastfetch/download"
 download_and_extract "$URL" "$HOME/opt/bin"
+
 URL="https://archlinux.org/packages/extra/x86_64/yyjson/download"
+download_and_extract "$URL" "$HOME/opt/bin"
+
+# Nano
+URL="https://archlinux.org/packages/core/x86_64/nano/download"
 download_and_extract "$URL" "$HOME/opt/bin"
 
 curl -L https://raw.githubusercontent.com/shadowed1/Aurora/beta/.flatpak.logic -o ~/opt/.flatpak.logic
@@ -141,6 +147,8 @@ curl -L https://raw.githubusercontent.com/shadowed1/Aurora/beta/.flatpak.env -o 
 
 chmod +x ~/opt/bin/aurora
 chmod +x ~/opt/bin/starman
+chmod +x ~/opt/bin/usr/bin/fastfetch
+chmod +x ~/opt/bin/usr/bin/nano
 
 
 export LD_LIBRARY_PATH="$HOME/opt/flatpak-deps/usr/lib:$LD_LIBRARY_PATH"
